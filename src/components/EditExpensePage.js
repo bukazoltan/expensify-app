deef6a99ac14bed4a0f4599fe71d0fc027cd8d6e
Zoltan Buka <bukazoltan@gmail.com>
2020-01-02 16:47:28 +0100

    added 404 and navbar

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditExpensePage = (props) => {
+  console.log(props)
+  return (
+  <div>
+    This is from EditExpensePage.
+  </div>);
+}
+
+export default EditExpensePage;
